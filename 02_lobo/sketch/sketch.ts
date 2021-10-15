856e5a1c92b3f8daf2e0e007e80b00c63c5f1067
ChicoMoreira <franciscomoreiran@alu.ufc.br>
2021-10-15 11:42:12 -0300

    atualizando lobinho

--- 02_lobo/sketch/sketch.ts
+++ 02_lobo/sketch/sketch.ts
@@ -1,22 +1,47 @@
-
-
 class Entity {
   x: number; //atributos
   y: number;
   step: number;
   image: p5.Image;
-  morreu: boolean;
-              //parametros
+  vivo: boolean;
+  renascer: number;
+                //parametros
   constructor(x: number, y: number, step: number, image: p5.Image) {
       this.x = x;
       this.y = y;
       this.step = step;
       this.image = image;
-      this.morreu = false;
+      this.vivo = true;
+      this.renascer = 0;
   }
+  
+  update(): void {
+    if (!this.vivo){
+      this.renascer--;
+      if (this.renascer <= 0) {
+        this.vivo = true;
+        this.renascer = 0;
+      }
+    }
+  }
+  
+  
+  
+  
+  
+  
+  
   //metodos
   draw(): void {
-      image(this.image, this.x * this.step, this.y * this.step, this.step, this.step);
+    if(!this.vivo) {
+      wolf.x = getRandomInt(1, 8);
+      wolf.y = getRandomInt(1, 5);
+      return;
+    
+
+    }
+  
+    image(this.image, this.x * this.step, this.y * this.step, this.step, this.step);
   }
 }
 
@@ -25,15 +50,17 @@ class Board {
   nc: number;
   step: number;
   background: p5.Image;
-
+  vivo: boolean;
   constructor(nc: number, nl: number, step: number, background: p5.Image) {
       this.nc = nc
       this.nl = nl
       this.step = step
       this.background = background;
+      this.vivo = true;
   }
 
   draw(): void {
+     
       image(this.background, 0, 0, this.nc * this.step, this.nl * this.step);
       for (let x = 0; x < this.nc; x++) {
           for (let y = 0; y < this.nl; y++) {
@@ -58,10 +85,6 @@ let wolf: Entity;
 let rabbit: Entity;
 
 let board: Board;
-
-
-
-
 
 
 function getRandomInt(min:number, max:number) {
@@ -90,6 +113,13 @@ function preload() {
 }
 
 function keyPressed() {
+  let wolf_x = wolf.x;
+  let wolf_y = wolf.y;
+  let rabbit_x = rabbit.x;
+  let rabbit_y = rabbit.y;
+  let trap_x = trap.x;
+  let trap_y = trap.y;
+
   if (keyCode === LEFT_ARROW ) {
       wolf.x--;
   } else if (keyCode === RIGHT_ARROW ) {
@@ -109,7 +139,32 @@ function keyPressed() {
   } else if (keyCode === "S".charCodeAt(0)) {
       rabbit.y++;
   }
+  
+  if(wolf.x == rabbit.x && wolf.y == rabbit.y){
+   // wolf.x = wolf_x;
+   // wolf.y = wolf_y;
+   // rabbit.x = rabbit_x;
+   // rabbit.y = rabbit_y;
+    rabbit.vivo = false;
+    rabbit.renascer = 30;
+    console.log("colisão");  
+  }
+
+  if(wolf.x == trap.x && wolf.y == trap.y){
+    console.log("lobo caiu na armadilha")
+    wolf. vivo = false;
+    wolf.renascer = 30;
+   // let wolf_x = getRandomInt(1, 8);
+   // let wolf_y = getRandomInt(1, 5); 
+  
+  }
+
+
+
 }
+
+
+ 
 
 
 
@@ -117,7 +172,7 @@ function keyPressed() {
 
 function setup() {
   let size = 100;
-  
+  frameRate(10);
   wolf = new Entity(2, 2, size, wolf_img);
   rabbit = new Entity(1, 1, size, rabbit_img);
   trap = new Entity(getRandomInt(1, 8), getRandomInt(1, 5), size, trap_img);
@@ -126,14 +181,17 @@ function setup() {
   createCanvas(board.nc * size, board.nl * size);
 }
 
-if (wolf.x === trap.x){
-  console.log("colisão");
-}
+
+
 
 
 function draw() {
+
   board.draw();
   trap.draw();
   wolf.draw();
   rabbit.draw(); 
+  
+  rabbit.update();
+  wolf.update();
 }
