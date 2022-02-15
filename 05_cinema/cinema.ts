dabc6aceccff3877ef777232584be193dcf1ecf1
ChicoMoreira <franciscomoreiran@alu.ufc.br>
2022-02-15 15:00:27 -0300

    Organização e otimização

--- 05_cinema/cinema.ts
+++ 05_cinema/cinema.ts
@@ -6,23 +6,25 @@ class Cliente {
         this.fone = fone;
     }
 
-    toString() {
-        return `${this.nome}, ${this.fone}`
-    }
-        
     setFone(fone : number) {
-       this.fone =  fone;
+        this.fone =  fone;
     }
-
+    
     setId(id : string) {
         this.nome = id;
     }
 
+    getNome() {
+        return this.nome
+    }
+    
+    toString() {
+        return `${this.nome}, ${this.fone}`
+    }
 }
 
 class Sala {
-    cadeiras : Array<Cliente | null>;
-    
+    cadeiras: Array<Cliente | null>;
     constructor (tamSala : number) {
         this.cadeiras = [];
         for (let i = 0; i < tamSala; i++) {
@@ -37,22 +39,15 @@ class Sala {
         if (cadeira == null) {
             sda += " - ";
         } else {
-            sda += ` ${cadeira.nome[i]} `;
-        }
-        
+            sda += ` ${cadeira.getNome()} `;
+        }  
     }   
     sda += ")";
     return sda;
     }
 
-    
-    //retorna -1 se não encontrar
-    indexOf(id: String): number {
+    indexOf(id: string): number {
         return this.cadeiras.findIndex(c => c != null && c.nome == id);
-        // for (let i = 0; i < this.cadeiras.length; i++)
-        //     if (this.cadeiras[i] != null && this.cadeiras[i].id == id)
-        //         return i;
-        // return -1;
     }
 
     reservar(cliente: Cliente, posicao: number): boolean {
@@ -72,50 +67,27 @@ class Sala {
         return true;
     }
 
-    cancelar(id: string): boolean {
+    cancelar(id: string) {
         let posicao = this.indexOf(id);
         if (posicao == -1) {
-            this.cadeiras[posicao] = null;
-            return true;
+            this.cadeiras[posicao] = null 
+            console.log(`${id} não encontrado na sessão`)
+            return;
         }
-        return false;
+        this.cadeiras[posicao] = null;
+        return;        
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-// let person = new Cliente("jober", 3000);
-
-// console.log(person.toString());
-// person.setFone(2999);
-// person.setId("jobervando");
-// console.log(person.toString());
-
-
-// let sala = new Sala(5);
-
-// console.log(sala.toString());
-
-// console.log("" + sala);
-
-
 let jober = new Cliente("jober", 3030);
-let leti = new Cliente("let", 30030);
+let leti = new Cliente("leti", 30030);
 let salaa = new Sala(5);
-
 salaa.reservar(jober, 1);
+salaa.reservar(new Cliente("goba", 1000), 4)
 salaa.reservar(leti, 1);
+salaa.reservar(new Cliente("doidim", 3434), 7)
 salaa.reservar(leti, 2);
-salaa.cancelar("jober");
-//console.log(control.abrir(5));
 console.log(salaa.toString());
-
+salaa.cancelar("goba");
+salaa.cancelar("chico")
+console.log(salaa.toString());
